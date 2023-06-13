d7c82fe6e0bb97cdae6239f8d188d25282fb1aeb
RogerWoellner <115326179+RogerWoellner@users.noreply.github.com>
2023-06-13 19:53:38 -0300

    Add files via upload

---
+++ Estudo C#/program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Primeiro
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo!");
+        }
+    }
+}
